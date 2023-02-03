f2bb02023273a6ff68461fa32515a7fc75c14d23
LexiisYing <nyxxing0824@gmail.com>
2023-02-03 19:06:15 +0000

    level 100 completed

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function makeSeasonAndEpisode(episode) {
+  const { season, number } = episode;
+  // the above is the same as the two lines below (this is from react)
+  // it's called OBJECT destructuring
+  // const season = episode.season;
+  // const number = episode.number;
+  const paddedSeason = season.toString().padStart(2, "0");
+  const paddedEpisode = number.toString().padStart(2, "0");
+  return `S${paddedSeason}E${paddedEpisode}`;
+
+}
+
 function makePageForEpisodes(allEpisodes) {
   const rootElem = document.getElementById("root");
   
     allEpisodes.forEach((episode) => {
       const paragraph = document.createElement("p");
-      paragraph.textContent = `S${episode.season}E${episode.number}:
+      paragraph.textContent = `${makeSeasonAndEpisode(episode)}:
       ${episode.name}`;
       rootElem.appendChild(paragraph);
       
@@ -17,10 +29,11 @@ function makePageForEpisodes(allEpisodes) {
       image.src = episode.image.medium;
       rootElem.appendChild(image);
       
+      rootElem.innerHTML += episode.summary;
+
 
     });
 
-  //rootElem.textContent = `Got ${allEpisodes.length} episode(s)`;
 }
 
 
