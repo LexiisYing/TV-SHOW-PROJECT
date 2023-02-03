b154c7b84cf145d159ae959371b61e1fceaf724f
LexiisYing <nyxxing0824@gmail.com>
2023-02-03 18:09:03 +0000

    initial page setup

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+function makePageForEpisodes(allEpisodes) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  
+    allEpisodes.forEach((episode) => {
+      const paragraph = document.createElement("p");
+      paragraph.textContent = `S${episode.season}E${episode.number}:
+      ${episode.name}`;
+      rootElem.appendChild(paragraph);
+      
+      const image = document.createElement("img");
+      image.src = episode.image.medium;
+      rootElem.appendChild(image);
+      
+
+    });
+
+  //rootElem.textContent = `Got ${allEpisodes.length} episode(s)`;
 }
+
 
 window.onload = setup;
