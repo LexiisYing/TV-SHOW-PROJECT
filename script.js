ae5afe08b6fa194044535ce2f091219cda7b9907
LexiisYing <nyxxing0824@gmail.com>
2023-02-12 06:41:29 +0000

    level 300, 350 completed

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+// //You can edit ALL of the code here
+// function setup() {
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
+// }
+
+const allShows = getAllShows();
+
+
+const rootElem = document.getElementById("root");
+async function setup() {
+  try {
+    const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+  let result = await response.json();
+    makePageForEpisodes(result);
+    episodeSelector(result);
+    searchItem(result);
+    displayingAllEpisodesLength(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 function makeSeasonAndEpisode(episode) {
   const { season, number } = episode;
@@ -60,3 +77,30 @@ searchInput.addEventListener("input", (event) => {
 
 
 window.onload = setup;
+
+const selectElement = document.getElementById("drop-menu");
+const allEpisodes = getAllEpisodes();
+for (let increment = 0; increment < allEpisodes.length; increment++) {
+  let movieOption = document.createElement("option");
+  const nameOfSeason = allEpisodes[increment].season;
+  const episodeNumber = allEpisodes[increment].number;
+  if (episodeNumber < 10) {
+    const episodeCodeDiv = `S0${nameOfSeason}E0${episodeNumber}`;
+    movieOption.value = allEpisodes[increment].name;
+    movieOption.text = episodeCodeDiv + " - " + allEpisodes[increment].name;
+  } else if (episodeNumber >= 10) {
+    const episodeCodeDiv = `S0${nameOfSeason}E${episodeNumber}`;
+    movieOption.value = allEpisodes[increment].name;
+    movieOption.text = episodeCodeDiv + " - " + allEpisodes[increment].name;
+  }
+  selectElement.appendChild(movieOption);
+}
+
+selectElement.addEventListener("change", (event) => {
+  const keyValue = event.target.value;
+  console.log(keyValue);
+  const filteredEpisode = allEpisodes.filter((episode) => {
+    return episode.name === keyValue;
+  })
+  makePageForEpisodes(filteredEpisode);
+});
